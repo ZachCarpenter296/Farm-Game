33485ba13982e68dabc2958441b990d5f75bb11d
ZachCarpenter296 <54647059+ZachCarpenter296@users.noreply.github.com>
2020-10-27 20:18:30 -0700

    Main Menu and Lobbies
    set up the main menu and made it to where players can make new lobbies or join existing ones

---
+++ Assets/Scripts/PhotonLobby.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Net.NetworkInformation;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks, ILobbyCallbacks
+{
+    public static PhotonLobby lobby;
+
+    public string roomName;
+    public GameObject roomListingPrefab;
+    public Transform roomsPanel;
+
+    [Tooltip("Max players per room")]
+    [SerializeField]
+    private int maxPlayers = 6;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the photon master server");
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+        RemoveRoomListings();
+        foreach(RoomInfo room in roomList)
+        {
+            ListRoom(room);
+        }
+    }
+    void RemoveRoomListings()
+    {
+        while(roomsPanel.childCount != 0)
+        {
+            Destroy(roomsPanel.GetChild(0).gameObject);
+        }
+    }
+
+    void ListRoom(RoomInfo room)
+    {
+        if(room.IsOpen && room.IsVisible)
+        {
+            GameObject tempListing = Instantiate(roomListingPrefab, roomsPanel);
+            RoomButton tempButton = tempListing.GetComponent<RoomButton>();
+            tempButton.roomName = room.Name;
+            tempButton.SetRoom();
+        }
+    }
+
+    public void CreateRoom()
+    {
+        Debug.Log("Trying to create a new room");
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)maxPlayers };
+        PhotonNetwork.CreateRoom(roomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Name taken");
+        //Add visual element
+    }
+
+    public void OnRoomNameChanged(string nameIn)
+    {
+        roomName = nameIn;
+    }
+
+    public void JoinLobbyOnClick()
+    {
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+}
