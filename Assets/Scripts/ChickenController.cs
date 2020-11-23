d00eff8393d6229ba0ce6ab4a1bdbd1b062afecb
Zetrix969 <zetrix969@gmail.com>
2020-11-23 00:06:23 -0700

    Update ChickenController.cs

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -6,208 +6,210 @@ using UnityEngine.UI;
 
 public class ChickenController : MonoBehaviour
 {
-    [SerializeField] GameObject cameraHolder;
+  [SerializeField] GameObject cameraHolder;
 
-    [SerializeField] float mouseSensitivity, walkSpeed, sprintSpeed, jumpForce, smoothTime;
+  [SerializeField] float mouseSensitivity, walkSpeed, sprintSpeed, jumpForce, smoothTime;
 
-    [SerializeField] Image powerfill;
+  [SerializeField] Image powerfill;
 
-    [SerializeField] GameObject playerUI;
+  [SerializeField] GameObject playerUI;
 
-    float verticalLookRotation;
-    bool grounded;
-    Vector3 smoothMoveVelocity;
-    Vector3 moveAmount;
+  float verticalLookRotation;
+  bool grounded;
+  Vector3 smoothMoveVelocity;
+  Vector3 moveAmount;
 
-    Rigidbody rb;
-    PhotonView PV;
+  Rigidbody rb;
+  PhotonView PV;
 
-    Animator myAnim;
-    float timer;
+  Animator myAnim;
+  float timer;
 
-    EnergyHealth myEnergyHealth;
-    Image health;
-    Image energy;
+  EnergyHealth myEnergyHealth;
+  Image health;
+  Image energy;
 
-    private void Awake()
+  private void Awake()
+  {
+    rb = GetComponent<Rigidbody>();
+    PV = GetComponent<PhotonView>();
+    myAnim = GetComponent<Animator>();
+  }
+
+  private void Start()
+  {
+    if (!PV.IsMine)
     {
-        rb = GetComponent<Rigidbody>();
-        PV = GetComponent<PhotonView>();
-        myAnim = GetComponent<Animator>();
+      Destroy(GetComponentInChildren<Camera>().gameObject);
+      Destroy(rb);
     }
 
-    private void Start()
+
+    else
     {
-        if (!PV.IsMine)
-        {
-            Destroy(GetComponentInChildren<Camera>().gameObject);
-            Destroy(rb);
-        }
+      GameObject myUI = Instantiate(playerUI);
+      myUI.transform.SetParent(this.transform, true);
 
-        //reset power bar and reset health 
-        Vector2 resetPower = new Vector2(0f, powerfill.rectTransform.transform.localScale.y);
+      //reset power bar and reset health 
+      Vector2 resetPower = new Vector2(0f, powerfill.rectTransform.transform.localScale.y);
 
-        powerfill.rectTransform.transform.localScale = resetPower;
-        else
-        {
-            GameObject myUI = Instantiate(playerUI);
-            myUI.transform.SetParent(this.transform, true);
+      powerfill.rectTransform.transform.localScale = resetPower;
 
-            //myEnergyHealth = myUI.GetComponent<EnergyHealth>();
-            //Image bar = myUI.GetComponentInChildren<Image>(0);
+      //myEnergyHealth = myUI.GetComponent<EnergyHealth>();
+      //Image bar = myUI.GetComponentInChildren<Image>(0);
 
-            //reset power bar and reset health 
-            //Vector2 resetPower = new Vector2(0f, powerfill.rectTransform.transform.localScale.y);
+      //reset power bar and reset health 
+      //Vector2 resetPower = new Vector2(0f, powerfill.rectTransform.transform.localScale.y);
 
-            //powerfill.rectTransform.transform.localScale = resetPower;
-            //Debug.LogWarning(powerfill.rectTransform.transform.localScale.x + " x value");
-        }
+      //powerfill.rectTransform.transform.localScale = resetPower;
+      //Debug.LogWarning(powerfill.rectTransform.transform.localScale.x + " x value");
+    }
+  }
+
+  private void Update()
+  {
+    if (!PV.IsMine)
+      return;
+    Look();
+    Jump();
+    Move();
+
+
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.tag == "Food")
+    {
+      //reset timer
+      timer = 0.0f;
+    }
+  }
+
+  private void OnTriggerStay(Collider other)
+  {
+    if (other.tag == "Food")
+    {
+      //Debug.Log("About to eat food");
+
+      Eat(other);
+    }
+  }
+
+  void Eat(Collider food)
+  {
+    if (Input.GetKeyDown(KeyCode.F))
+    {
+      myAnim.SetBool("isEating", true);
     }
 
-    private void Update()
+    if (Input.GetKey(KeyCode.F))
     {
-        if (!PV.IsMine)
-            return;
-        Look();
-        Jump();
-        Move();
+      Debug.Log("EATING");
+      //Debug.Log(timer);
 
-        
+      timer += Time.deltaTime;
+
+      if (timer >= 1.99f && timer <= 2.01f)
+      {
+        //Debug.Log("ATE CABBAGE 1");
+        //Update the power bar
+        Vector2 currentSize = powerfill.rectTransform.transform.localScale;
+        Vector2 power = new Vector2(0.1f, 0);
+        powerfill.rectTransform.transform.localScale = currentSize + power;
+
+        GameObject lettuce = food.gameObject.transform.Find("Lettuce2").gameObject;
+
+        Destroy(lettuce);
+      }
+      else if (timer >= 3.99f && timer <= 4.01f)
+      {
+        //Debug.Log("ATE CABBAGE 2");
+        //Update the power bar
+        Vector2 currentSize = powerfill.rectTransform.transform.localScale;
+        Vector2 power = new Vector2(0.1f, 0);
+        powerfill.rectTransform.transform.localScale = currentSize + power;
+
+        GameObject lettuce = food.gameObject.transform.Find("Lettuce4").gameObject;
+
+        Destroy(lettuce);
+      }
+      else if (timer >= 5.99f && timer <= 6.01f)
+      {
+        //Debug.Log("ATE CABBAGE 3");
+        //Update the power bar
+        Vector2 currentSize = powerfill.rectTransform.transform.localScale;
+        Vector2 power = new Vector2(0.1f, 0);
+        powerfill.rectTransform.transform.localScale = currentSize + power;
+
+        GameObject lettuce = food.gameObject.transform.Find("Lettuce6").gameObject;
+
+        Destroy(lettuce);
+      }
     }
-
-    private void OnTriggerEnter(Collider other)
+    if (Input.GetKeyUp(KeyCode.F))
     {
-        if(other.tag == "Food")
-        {
-            //reset timer
-            timer = 0.0f;
-        }
+      myAnim.SetBool("isEating", false);
     }
+  }
 
-    private void OnTriggerStay(Collider other)
+  void TakeDamage(float damage)
+  {
+    Debug.Log("Taking damage");
+  }
+
+  //Controls for the farmers camera
+  void Look()
+  {
+    transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+    verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+    verticalLookRotation = Mathf.Clamp(verticalLookRotation, -75f, 75f);
+
+    cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+
+  }
+
+  //Controls for the farmers movement
+  void Move()
+  {
+    Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+    moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+  }
+
+  //Controls for the farmers jumping
+  void Jump()
+  {
+    if (Input.GetKeyDown(KeyCode.Space) && grounded)
     {
-        if(other.tag == "Food")
-        {
-            //Debug.Log("About to eat food");
-
-            Eat(other);
-        }
+      rb.AddForce(transform.up * jumpForce);
     }
+  }
 
-    void Eat(Collider food)
+  //Bool to see if the farmer is grounded, if yes, then can jump
+  public void SetGroundedState(bool _grounded)
+  {
+    grounded = _grounded;
+  }
+
+  private void FixedUpdate()
+  {
+    if (!PV.IsMine)
+      return;
+
+    rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+
+
+    //check to see if there is movement input from user, if so
+    //set animator boolean isRunning to true
+    if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            myAnim.SetBool("isEating", true);
-        }
-
-        if (Input.GetKey(KeyCode.F))
-        {
-            Debug.Log("EATING");
-            //Debug.Log(timer);
-
-            timer += Time.deltaTime;
-
-            if (timer >= 1.99f && timer <= 2.01f)
-            {
-                //Debug.Log("ATE CABBAGE 1");
-                //Update the power bar
-                Vector2 currentSize = powerfill.rectTransform.transform.localScale;
-                Vector2 power = new Vector2(0.1f, 0);
-                powerfill.rectTransform.transform.localScale = currentSize + power;
-
-                GameObject lettuce = food.gameObject.transform.Find("Lettuce2").gameObject;
-
-                Destroy(lettuce);
-            }
-            else if(timer >= 3.99f && timer <= 4.01f)
-            {
-                //Debug.Log("ATE CABBAGE 2");
-                //Update the power bar
-                Vector2 currentSize = powerfill.rectTransform.transform.localScale;
-                Vector2 power = new Vector2(0.1f, 0);
-                powerfill.rectTransform.transform.localScale = currentSize + power;
-
-                GameObject lettuce = food.gameObject.transform.Find("Lettuce4").gameObject;
-
-                Destroy(lettuce);
-            }
-            else if (timer >= 5.99f && timer <= 6.01f)
-            {
-                //Debug.Log("ATE CABBAGE 3");
-                //Update the power bar
-                Vector2 currentSize = powerfill.rectTransform.transform.localScale;
-                Vector2 power = new Vector2(0.1f, 0);
-                powerfill.rectTransform.transform.localScale = currentSize + power;
-
-                GameObject lettuce = food.gameObject.transform.Find("Lettuce6").gameObject;
-
-                Destroy(lettuce);
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            myAnim.SetBool("isEating", false);
-        }
+      myAnim.SetBool("isRunning", true);
     }
-
-    void TakeDamage(float damage)
+    else if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
     {
-        Debug.Log("Taking damage");
+      myAnim.SetBool("isRunning", false);
     }
-
-    //Controls for the farmers camera
-    void Look()
-    {
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
-
-        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
-        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -75f, 75f);
-
-        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
-
-    }
-
-    //Controls for the farmers movement
-    void Move()
-    {
-         Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-
-         moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);      
-    }
-
-    //Controls for the farmers jumping
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-        {
-            rb.AddForce(transform.up * jumpForce);
-        }
-    }
-
-    //Bool to see if the farmer is grounded, if yes, then can jump
-    public void SetGroundedState(bool _grounded)
-    {
-        grounded = _grounded;
-    }
-
-    private void FixedUpdate()
-    {
-        if (!PV.IsMine)
-            return;
-
-        rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
-
-
-        //check to see if there is movement input from user, if so
-        //set animator boolean isRunning to true
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-        {
-            myAnim.SetBool("isRunning", true);
-        }
-        else if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
-        {
-            myAnim.SetBool("isRunning", false);
-        }
-    }
+  }
 }
